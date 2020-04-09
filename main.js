c8443302e72e3d3242aef9b1ff6f027592788952
Ali <a.dadawov@outlook.com>
2020-04-09 13:05:27 +0300

    home work

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-var money;
-var time;
+'use strict';
+
+let money,
+    time,
+    expens,
+    moneyExpens,
+    everyDay;
+
+let appData = {
+    budget: 0,
+    timeData: '',
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
